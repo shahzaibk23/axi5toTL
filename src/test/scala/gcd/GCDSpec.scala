59781ad1155a1146455a5755231c2e2d37dde71f
GitHub Action <action@github.com>
2024-04-08 21:31:56 +0000

    Template cleanup

--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -20,7 +20,7 @@ import org.scalatest.matchers.must.Matchers
   * }}}
   * Testing from mill:
   * {{{
-  * mill %NAME%.test.testOnly gcd.GCDSpec
+  * mill axi5toTL.test.testOnly gcd.GCDSpec
   * }}}
   */
 class GCDSpec extends AnyFreeSpec with Matchers {
